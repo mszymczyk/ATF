aea2f980118d85a79d25db63d6ac22c47087a91c
mszymczyk <misz.misz.misz@gmail.com>
2015-09-23 10:22:54 +0200

    poprawki w wybieralce plikow
    pierwsze uzycie edytora powodowalo ustawienie initialDirectory w zlym
    miejscu
    dodalem customplaces: PICO_DEMO\data i PICO_DEMO\data\assets

--- Samples/pico/Controls/PropertyEditing/picoFileUriEditor.cs
+++ Samples/pico/Controls/PropertyEditing/picoFileUriEditor.cs
@@ -1,6 +1,4 @@
-﻿//Copyright © 2014 Sony Computer Entertainment America LLC. See License.txt.
-
-using System;
+﻿using System;
 using System.IO;
 using System.Text;
 using System.Windows.Forms;
@@ -12,8 +10,11 @@ using Sce.Atf.Controls.PropertyEditing;
 namespace pico.Controls.PropertyEditing
 {
     /// <summary>
-    /// UITypeEditor class for editing file paths (stored as strings or URIs)</summary>
-	public class picoFileUriEditor : FileUriEditor
+    /// UITypeEditor class for editing file paths (stored ONLY as strings)
+	/// This class is for selecting file only within PICO_DEMO file system hierarchy
+	/// It's mainly a copy of ATF's FileUriEditor with modifications to handle relative path names
+	/// </summary>
+    public class picoFileUriEditor : FileNameEditor, IAnnotatedParams
     {
         /// <summary>
         /// Constructor</summary>
@@ -25,11 +26,66 @@ namespace pico.Controls.PropertyEditing
         /// Constructor with file filter</summary>
         /// <param name="filter">Filter string for open file dialog</param>
         public picoFileUriEditor(string filter)
-			: base( filter )
         {
+            m_filter = filter;
         }
 
-		/// <summary>
+        /// <summary>
+        /// Gets or sets the filter string for the open file dialog</summary>
+        public string Filter
+        {
+            get { return m_filter; }
+            set { m_filter = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the full path of the text editor associated to view the file contents</summary>
+        public string AssociatedTextEditor
+        {
+            get
+            {
+                return string.IsNullOrEmpty(m_associatedTextEditor) ? GlobalDefaultTextEditor : m_associatedTextEditor;
+            }
+
+            set { m_associatedTextEditor = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the default text editor for viewng file contents</summary>
+        public static string GlobalDefaultTextEditor
+        {
+            get { return s_globalDefaultTextEditor; }
+            set { s_globalDefaultTextEditor = value; }
+        }
+
+        #region IAnnotatedParams Members
+
+        /// <summary>
+        /// Initializes the control</summary>
+        /// <param name="parameters">Editor parameters</param>
+        public void Initialize(string[] parameters)
+        {
+            if (parameters.Length == 1)
+            {
+                m_filter = parameters[0];
+            }
+            else if (parameters.Length > 1)
+            {
+                // This editor only accept on parameter.
+                // If there are more than one, then combine them into one.
+                StringBuilder filter = new StringBuilder();
+                for (int i = 0; i < parameters.Length - 1; i++)
+                {
+                    filter.AppendFormat("{0},", parameters[i]);
+                }
+                filter.Append(parameters[parameters.Length - 1]);
+                m_filter = filter.ToString();                
+            }                        
+        }
+
+        #endregion
+
+        /// <summary>
         /// Displays a file open dialog box to allow the user to edit the specified path (value).
         /// Sets the initial directory of the file open dialog box to be the path, if it's valid.</summary>
         /// <param name="context">Type descriptor context</param>
@@ -49,23 +105,12 @@ namespace pico.Controls.PropertyEditing
             {
                 // Try to set the initial directory to be the path that's in 'value'.
                 // Also, fix up the path by removing forward slashes. This is critical for Windows XP.
-                m_initialDirectory2 = null;
+                m_initialDirectory = null;
 
                 // Can't use the property descriptor to convert to a string because we need the LocalPath
                 //  if possible.
                 //string path = context.PropertyDescriptor.Converter.ConvertToString(value);
                 string path = value as string;
-                if (path == null)
-                {
-                    Uri uri = value as Uri;
-                    if (uri != null)
-                    {
-                        if (uri.IsAbsoluteUri)
-                            path = uri.LocalPath;
-                        else
-                            path = uri.OriginalString;
-                    }
-                }
 
                 if (!string.IsNullOrEmpty(path))
                 {
@@ -79,37 +124,32 @@ namespace pico.Controls.PropertyEditing
                     {
                         string directory = Path.GetDirectoryName(fixedPath);
                         if (!string.IsNullOrEmpty(directory))
-                            m_initialDirectory2 = directory;
+                            m_initialDirectory = directory;
+
+						string filename = Path.GetFileName( fixedPath );
+						value = filename;
                     }
                 }
 
-                if (m_dialog2 != null &&
-                    !string.IsNullOrEmpty(m_initialDirectory2))
-                    m_dialog2.InitialDirectory = m_initialDirectory2;
+                if (m_dialog != null &&
+                    !string.IsNullOrEmpty(m_initialDirectory))
+                    m_dialog.InitialDirectory = m_initialDirectory;
 
-				object newValue = base.EditValue(context, provider, value);
-				if ( newValue.Equals(value) )
-					return newValue;
+                object newValue = base.EditValue(context, provider, value);
 
 				string newPath = newValue as string;
-				if (newPath == null)
+
+				if ( !string.IsNullOrEmpty( newPath ) )
 				{
-					Uri uri = newValue as Uri;
-					if (uri != null)
-					{
-						if (uri.IsAbsoluteUri)
-							newPath = uri.LocalPath;
-						else
-							newPath = uri.OriginalString;
-					}
+					if ( m_initialDirectory != null )
+						newPath = Path.Combine( m_initialDirectory, newPath );
+
+					string picoDemoPath = Paths.PathToPicoDemoPath( newPath );
+					return picoDemoPath;
 				}
 
-				string picoDemoPath = Paths.PathToPicoDemoPath( newPath );
-				return picoDemoPath;
-				//Uri newUri = new Uri( picoDemoPath, UriKind.Relative );
-				//return newUri;
-				//return newPath;
-			}
+				return string.Empty;
+            }
             catch (Exception e)
             {
                 Outputs.WriteLine(OutputMessageType.Error, e.Message);
@@ -117,17 +157,29 @@ namespace pico.Controls.PropertyEditing
             }
         }
 
-		/// <summary>
-		/// Initializes the open file dialog</summary>
-		/// <param name="dialog">The open file dialog.</param>
-		protected override void InitializeDialog( OpenFileDialog dialog )
-		{
-			base.InitializeDialog( dialog );
+        /// <summary>
+        /// Initializes the open file dialog</summary>
+        /// <param name="dialog">The open file dialog.</param>
+        protected override void InitializeDialog(OpenFileDialog dialog)
+        {
+            base.InitializeDialog(dialog);
+            
+            if (m_filter != null)
+                dialog.Filter = m_filter;
 
-			m_dialog2 = dialog;
+            m_dialog = dialog;
+            if (!string.IsNullOrEmpty(m_initialDirectory))
+                m_dialog.InitialDirectory = m_initialDirectory;
+
+			m_dialog.CustomPlaces.Add( pico.Paths.PICO_DEMO_data );
+			m_dialog.CustomPlaces.Add( pico.Paths.PICO_DEMO_data + "assets" );
 		}
 
-		private string m_initialDirectory2;
-		private OpenFileDialog m_dialog2;
+        private static string s_globalDefaultTextEditor = @"notepad.exe";
+
+        private string m_filter;
+        private string m_initialDirectory;
+        private OpenFileDialog m_dialog;
+        private string m_associatedTextEditor;
      }
 }
